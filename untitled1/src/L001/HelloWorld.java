a699830b1f58881648141dd74eda8243de602a93
Dekiston <doolino14@gmail.com>
2022-06-19 18:01:57 +0300

    L001-Hello world!

---
+++ untitled1/src/L001/HelloWorld.java
@@ -0,0 +1,7 @@
+package L001;
+
+public class HelloWorld {
+    public static void run(){
+        System.out.println("Hello, world!");
+    }
+}
--- untitled1/src/Main.java
+++ untitled1/src/Main.java
@@ -1,5 +1,8 @@
+import L001.HelloWorld;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HelloWorld.run();
     }
+
 }
